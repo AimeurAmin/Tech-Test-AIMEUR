797bb2cf8a46c28f299293e13e6093009f253889
Amin AIMEUR <m.amin.aimeur@gmail.com>
2021-05-31 18:16:33 +0100

    creating first card

--- functions.js
+++ functions.js
@@ -1,1 +1,13 @@
 
+
+// document.addEventListener('DOMContentLoaded', (event) => {
+//   //the event occurred
+//   var elements = document.getElementsByClassName('someclass')[0].innerHTML;
+//   console.log(parseInt(elements));
+//   testFunction(parseInt(elements));
+// })
+
+
+// function testFunction( value ) {
+//   console.log(value + 1);
+// }
